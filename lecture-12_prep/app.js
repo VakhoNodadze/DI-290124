48015fb3d4a5623a2f1ce7ea5758197dfede63f4
Vakho Nodadze <vakhtang.nodadze@gmail.com>
2024-03-04 18:34:13 +0400

    HTML&CSS

---
+++ lecture-12_prep/app.js
@@ -0,0 +1,69 @@
+// var myName = "Vakhtang"; // var იგივეა რაც variable
+// var mySurname = "Nodadze";
+// var myAge = 27;
+
+// var isDeveloper = true;
+// var isStudent = false;
+
+// var myObject;
+// var myObject2 = null;
+
+// console.log(myName);
+// console.log(mySurname);
+// console.log(myAge);
+// console.log(isDeveloper);
+// console.log(isStudent);
+// console.log(myObject);
+// console.log(myObject2);
+
+// var numberString = "42";
+// var number = 42;
+
+// // console.log(numberString);
+// // console.log(number);
+// // console.log(myObject == myObject2);
+// // console.log(numberString == number);
+
+// // var userName = prompt("Enter your name");
+// // var userAge = prompt("Enter your age");
+// // var isStudyingJs = confirm("Are you studying JS?");
+
+// // document.write("Hello" + userName, ". You are " + userAge + " years old. ");
+// // document.write(
+// //   isStudyingJs ? "You are studying JS" : "You are not studying JS"
+// // );  // ეს იგივეა რაც ქვედა ხაზზე. ოღონდ უფრო ვრცლად
+
+// if (isStudyingJs) {
+//   document.write("You are studying JS");
+// } else {
+//   document.write("You are not studying JS");
+// }
+
+// var myAge = 19;
+// var mySex = "Male";
+// var myName = "Davit";
+// if ((myName == "Vakhtang" && myAge > 18) || mySex === "Male") {
+//   console.log("You are either Vakhtang AND over 18 OR you are male");
+// } else {
+//   console.log("You are not Vakhtang");
+// }
+
+// if (myAge !== 18) {
+//   console.log("You are not 18");
+// }
+
+// დავალება: ბრაუზერში იუზერმა უნდა შეიყვანოს საკუთარი სახელი და დაბადების წელი. დაიანგარიშეთ მისი ასაკი. თუ იგი არის
+// 21 წლის ან მეტის მაშინ დაუბეჭდეთ რომ მისი კლუბში შესვლა დაშვებულია. (ანუ თუ 21 ან მეტისაა, აუცილებლად "უშვებთ კლუბში"). ხოლო თუ მისი სახელი არის დავითი ან ნინო მაშინ დაუბეჭდეთ კიდევ რომ ის არის VIP კლიენტი. ხოლო თუ მას ეს არ ჰქვია, უთხარით რომ ის ჩვეულებრივი კლიენტია.
+
+// var userName = prompt('Enter your name');
+// var userBirthYear = prompt('Enter your birth year');
+// var userAge = 2023 - userBirthYear;
+
+// if (userAge >= 21) {
+//   console.log('You are allowed to enter the club');
+//   if (userName === 'Davit' || userName === 'Nino') {
+//     console.log('You are VIP client');
+//   } else {
+//     console.log('You are regular client');
+//   }
+// }
